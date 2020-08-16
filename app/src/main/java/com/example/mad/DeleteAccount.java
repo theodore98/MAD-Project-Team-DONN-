f236bdbd0ee1e4dfd77e900298c5a3c36da8e24e
theodore98 <tnaveendesilva@gmail.com>
2020-08-16 22:03:34 +0530

    IT18226874-MAD Project

---
+++ app/src/main/java/com/example/mad/DeleteAccount.java
@@ -0,0 +1,14 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}
