750e2dfff3014b4822937c2fe38827efb28f031b
theodore98 <tnaveendesilva@gmail.com>
2020-10-02 12:00:59 +0530

    Update 2.1(Business Registration)

--- app/src/main/java/com/example/food2u/MainActivity.java
+++ app/src/main/java/com/example/food2u/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.food2u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button register,login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=(Button)findViewById(R.id.loginbtn);
+        register=(Button)findViewById(R.id.regbtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login=new Intent(MainActivity.this,LoginBusiness.class);
+                startActivity(login);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent register=new Intent(MainActivity.this,RegisterBusiness.class);
+               startActivity(register);
+
+            }
+        });
+
+
     }
 }
