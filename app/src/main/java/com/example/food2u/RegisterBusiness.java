8577bd8df596e611893c90e19fd7ec35d6820e0f
theodore98 <tnaveendesilva@gmail.com>
2020-10-02 19:09:54 +0530

    Update 2.3(Business Registration-contd)

--- app/src/main/java/com/example/food2u/RegisterBusiness.java
+++ app/src/main/java/com/example/food2u/RegisterBusiness.java
@@ -2,13 +2,29 @@ package com.example.food2u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterBusiness extends AppCompatActivity {
+    ImageButton home;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_business);
+
+        home=(ImageButton)findViewById(R.id.homeBtn);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home= new Intent(RegisterBusiness.this,MainActivity.class);
+                startActivity(home);
+            }
+        });
     }
 }
