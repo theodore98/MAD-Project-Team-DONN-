af325878835eb8ed80a949996430bcda665353d5
theodore98 <tnaveendesilva@gmail.com>
2020-10-02 10:27:43 +0530

    Update 2(Business Registration)

---
+++ app/src/main/java/com/example/food2u/RegisterBusiness.java
@@ -0,0 +1,14 @@
+package com.example.food2u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterBusiness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_business);
+    }
+}
