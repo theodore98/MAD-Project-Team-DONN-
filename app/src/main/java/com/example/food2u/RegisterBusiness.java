0626f17a8b66b345f4b5003fcbd8e546644c40b7
theodore98 <tnaveendesilva@gmail.com>
2020-10-03 05:54:37 +0530

    Update 3.0(Business Home)

--- app/src/main/java/com/example/food2u/RegisterBusiness.java
+++ app/src/main/java/com/example/food2u/RegisterBusiness.java
@@ -9,6 +9,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -22,7 +23,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
+
 import java.util.HashMap;
+
+
 
 public class RegisterBusiness extends AppCompatActivity {
     ImageButton home;
@@ -49,7 +54,7 @@ public class RegisterBusiness extends AppCompatActivity {
         bank=(TextInputLayout) findViewById(R.id.Bank);
         acNumber=(TextInputLayout) findViewById(R.id.AcNumber);
 
-        registerBtn=(Button)findViewById(R.id.regbtn);
+        registerBtn=(Button)findViewById(R.id.regSubmit);
 
 
 
@@ -84,7 +89,7 @@ public class RegisterBusiness extends AppCompatActivity {
                                 hashMap1.put("Owner",OWNER);
                                 hashMap1.put("Bank",BANK);
                                 hashMap1.put("Account Number",ACNUMBER);
-                                hashMap1.put("Email",EMAIL;
+                                hashMap1.put("Email",EMAIL);
                                 hashMap1.put("Password",PASSWORD);
                                 hashMap1.put("Address",ADDRESS);
 
@@ -113,43 +118,22 @@ public class RegisterBusiness extends AppCompatActivity {
                                                     Alert.show();
                                                 }else{
                                                     mDialog.dismiss();
-                                                    ReusableCodeForAll.ShowAlert(ChefRegistration.this,"Error",task.getException().getMessage());
+                                                    ReusableCodeForAll.ShowAlert(RegisterBusiness.this,"Error",task.getException().getMessage());
                                                 }
 
                                             }
-                                        })
+                                        });
                                     }
-                                })
+                                });
 
 
-                                }
-                            });
-                        }
+                            }
+                        });
                     }
-
-
             }
+
+
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         home=(ImageButton)findViewById(R.id.homeBtn);
@@ -160,5 +144,109 @@ public class RegisterBusiness extends AppCompatActivity {
                 startActivity(home);
             }
         });
+
+
+
+
+
+
+
+
     }
+
+
+    String emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    public boolean isValid(){
+        email.setErrorEnabled(false);
+        email.setError("");
+
+        name.setErrorEnabled(false);
+        name.setError("");
+
+        mobile.setErrorEnabled(false);
+        mobile.setError("");
+
+        password.setErrorEnabled(false);
+        password.setError("");
+
+        owner.setErrorEnabled(false);
+        owner.setError("");
+
+        bank.setErrorEnabled(false);
+        bank.setError("");
+
+        acNumber.setErrorEnabled(false);
+        acNumber.setError("");
+
+        address.setErrorEnabled(false);
+        address.setError("");
+
+        boolean isValid = false, isValidemail = false, isValidname = false, isValidaddress = false, isValidmobile = false, isValidpassword = false, isValidcbank = false, isValidacNumber = false, isValidowner = false;
+        if (TextUtils.isEmpty(NAME)) {
+            name.setErrorEnabled(true);
+            name.setError("Enter Name");
+        } else {
+            isValidname = true;
+        }
+        if (TextUtils.isEmpty(ADDRESS)) {
+            address.setErrorEnabled(true);
+            address.setError("Enter Name");
+        } else {
+            isValidaddress = true;
+        }
+        if (TextUtils.isEmpty(MOBILE)) {
+            mobile.setErrorEnabled(true);
+            mobile.setError("Enter MObile");
+        } else {
+            isValidmobile = true;
+        }
+        if (TextUtils.isEmpty(PASSWORD)) {
+            password.setErrorEnabled(true);
+            password.setError("Enter Password");
+        } else {
+            isValidpassword = true;
+        }
+        if (TextUtils.isEmpty(BANK)) {
+            bank.setErrorEnabled(true);
+            bank.setError("Enter Bank");
+        } else {
+            isValidcbank = true;
+        }
+        if (TextUtils.isEmpty(ACNUMBER)) {
+            acNumber.setErrorEnabled(true);
+            acNumber.setError("Enter Account Number");
+        } else {
+            isValidacNumber = true;
+        }
+        if (TextUtils.isEmpty(OWNER)) {
+            owner.setErrorEnabled(true);
+            owner.setError("Enter Owner");
+        } else {
+            isValidowner = true;
+        }
+        if(TextUtils.isEmpty(EMAIL)){
+            email.setErrorEnabled(true);
+            email.setError("Email Is Required");
+        }else {
+            if (EMAIL.matches(emailpattern)) {
+                isValidemail = true;
+            } else {
+                email.setErrorEnabled(true);
+                email.setError("Enter a Valid Email Id");
+            }
+        }
+
+
+        isValid = (isValidname && isValidpassword && isValidemail && isValidaddress && isValidmobile && isValidowner && isValidcbank && isValidacNumber) ? true : false;
+        return isValid;
+    }
+
+
+
+
+
+
+
+
+
 }
